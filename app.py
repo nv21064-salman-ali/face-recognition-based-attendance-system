617bc3c0380686dd9619e8aea5f9d12f4b69107d
abdelrahman-khaled-nvtc <118051713+abdelrahman-khaled-nvtc@users.noreply.github.com>
2023-04-11 12:48:14 +0300

    Added different routing/ UI
    Added different routing for taking attendance and adding users. Also implemented basic UI interface.

--- app.py
+++ app.py
@@ -198,12 +198,15 @@ def start():
     cap.release()
     cv2.destroyAllWindows()
     names, rolls, times, l = extract_attendance()    
-    return render_template('home.html', names=names, rolls=rolls, times=times, l=l, totalreg=totalreg(), datetoday2=datetoday2)
+    return render_template('takeAttendance.html', names=names, rolls=rolls, times=times, l=l, totalreg=totalreg(), datetoday2=datetoday2)
 
 
 #### This function will run when we add a new user
 @app.route('/add',methods=['GET','POST'])
 def add():
+    if request.method == 'GET':
+        return render_template('addUser.html', totalreg=totalreg(), datetoday2=datetoday2)
+    
     newusername = request.form['newusername']
     newuserid = request.form['newuserid']
     userimagefolder = 'static/faces/'+newusername+'_'+str(newuserid)
@@ -232,7 +235,7 @@ def add():
     print('Training Model')
     train_model()
     names,rolls,times,l = extract_attendance()    
-    return render_template('home.html',names=names,rolls=rolls,times=times,l=l,totalreg=totalreg(),datetoday2=datetoday2) 
+    return render_template('addUser.html',names=names,rolls=rolls,times=times,l=l,totalreg=totalreg(),datetoday2=datetoday2) 
 
 
 #### Our main function which runs the Flask App
